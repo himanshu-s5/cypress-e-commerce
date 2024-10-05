fc0cf2f6671f0a821e3781387742c880fa996a96
himanshu-s5 <himanshusharma.w9@gmail.com>
2024-10-05 15:48:05 +0530

    modified

--- cypress/pageObject/CartPage.js
+++ cypress/pageObject/CartPage.js
@@ -1,20 +1,39 @@
 class Cart{
 
+
+    cartCount(){
+
+        return cy.get('#nav-cart-count').invoke('text').then((text)=>parseInt(text))
+
+    }
+    
     addToCart(){
 
-        let numberOfItem = cy.get('#nav-cart-count').invoke('text')
-        cy.get('#a-autoid-3-announce').trigger('scroll')
-        cartbtn.click()
-        let currentNumberOfItem = cy.get('#nav-cart-count').invoke('text')
-        expect(parseInt(numberOfItem) < parseInt(currentNumberOfItem))
-        
+        this.cartCount().then((numberOfItem)=>{
+        cy.get('#a-autoid-3-announce').scrollIntoView()
+        .click()
+
+        cy.get('#nav-cart-count').scrollIntoView().should('be.visible',{timeout:2000})
+        .then(()=>{
+            cy.wait(2000)
+            this.cartCount().then((currentNumberOfItem)=>{
+                expect(parseInt(currentNumberOfItem)).to.be.greaterThan(parseInt(numberOfItem))
+    
+        })
+       
+        })
+        })
+       
     }
 
     deleteCart(){
-
-        cy.get('#a-autoid-3-announce').click()
+        cy.get('#nav-cart-count').click()
         cy.get("input[value='Delete']").click()
-        cy.get('#a-autoid-3-announce').should('have.text','0')
+        cy.get('.a-size-extra-large').should('be.visible').then(()=>{
+            cy.get('.a-size-extra-large').contains('empty')
+
+        })
+           
     }
 
     }
